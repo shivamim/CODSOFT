d879c34a2aaf0b711292ac8531bc98bc0ea08b05
Shivam Shukla <110848969+shivamim@users.noreply.github.com>
2023-09-04 21:36:07 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -4,9 +4,8 @@ from utils import PrepProcesor, columns
 import numpy as np
 import pandas as pd
 import joblib
-import pickle 
-
-model = joblib.load('xgbpipe.joblib')
+import pickle
+model= pickle.load(open('file.pkl', 'rb'))
 st.title('Did they survive? :ship:')
 # PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 passengerid = st.text_input("Input Passenger ID", '123456') 
